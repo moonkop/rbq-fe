83e1270f126d4400908cf9ae0c13dc760d01408c
moonkop <moonkop@qq.com>
2020-01-28 11:52:48 +0800

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React, {Fragment} from 'react'
-import {Router, Route, Switch} from 'react-router-dom'
-import { createBrowserHistory } from "history"
+import React from 'react'
+import {Route, Router, Switch} from 'react-router-dom'
 import {Navbar} from './components/Navbar'
 import {About} from './pages/About'
 import {ArticleListPage} from "./pages/ArticleList";
 import {EditPage} from "./pages/Edit";
 import {history} from "./history";
+import {LoginPage} from "./pages/Login";
 
 export const App = () => (
 	<Router history={history}>
@@ -15,6 +15,7 @@ export const App = () => (
 				<Route path="/" component={ArticleListPage} exact/>
 				<Route path="/edit/:name" component={EditPage}/>
 				<Route path="/about" component={About}/>
+				<Route path="/login" component={LoginPage}/>
 			</Switch>
 		</div>
 	</Router>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,11 @@
-import React, {ChangeEvent, ChangeEventHandler, MouseEventHandler} from "react";
-import {computed, observable} from "mobx";
+import React, {ChangeEvent} from "react";
 
 export const LoginPage: React.FC = () => {
 	return <div className='page-login'>
 		<Login/>
 	</div>
-}
+};
 
-class LoginStore {
-	@observable name: string = ''
-
-}
 
 const Login: React.FC = () => {
 	const [inputValue, setInputValue] = React.useState('');
@@ -30,7 +25,7 @@ const Login: React.FC = () => {
 		       }
 		       }/>
 		<div>你输入的是{inputValue}</div>
-		<button className='login-button' onClick={()=>{
+		<button className='login-button' onClick={() => {
 
 		}}>
 			我要进去！
@@ -39,37 +34,4 @@ const Login: React.FC = () => {
 			（不同的人进去看到的东西也不一样哦
 		</div>
 	</div>;
-}
-// export class Login extends React.Component<any, any> {
-// 	componentWillMount() {
-//
-// 	}
-//
-// 	render() {
-// 		return <div>
-// 			<div>
-// 				<p>
-// 					(这里有很多不可告人的秘密)
-// 				</p>
-// 				<p>
-// 					来吧告诉我你是哪只？（或者我怎么称呼你？）
-// 				</p>
-// 			</div>
-// 			<input type="text" className='input-main' onChange={this.handleInput}/>
-// 			<button className='login-button' onClick={this.handleSubmit}>
-// 				我要进去！
-// 			</button>
-// 			<div>
-// 				（不同的人进去看到的东西也不一样哦
-// 			</div>
-// 		</div>
-// 	}
-//
-// 	private handleSubmit() {
-//
-// 	}
-//
-// 	private handleInput(event: React.ChangeEvent<HTMLInputElement>) {
-//
-// 	}
-// }
+};
